53c2ee28966d7c67cf949115d727e9a6d195c1c2
Puspendu <pb249@njit.edu>
2018-10-29 12:56:19 +0530

    Update src/cms/preview-templates/ContactpagePreview.js

--- src/cms/preview-templates/ContactpagePreview.js
+++ src/cms/preview-templates/ContactpagePreview.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { ContactPageTemplate } from "../../templates/contact-page";
 
-const ContactPagePreview = ({ entry, getAsset }) => {
+const ContactPagePreview = ({ entry }) => {
   return (
     <ContactPageTemplate
       title={entry.getIn(["data", "title"])}
